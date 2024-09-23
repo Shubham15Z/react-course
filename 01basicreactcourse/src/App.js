1c9139646c73c796e4bf43eefe7690c023dc0294
Shubham Zungure <zungureshubham1@gmail.com>
2024-09-23 20:11:45 +0530

    created two project

---
+++ 01basicreactcourse/src/App.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello"
+
+function App() {
+  return (
+    <>
+    <Hello/>
+    <h1>Hello World</h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Hello from "./hello"
+
+function App() {
+
+
+  return (
+    <>
+    <Hello />
+    <h1>This is h1 tag</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+    </>
+  )
+}
+
+export default App
